1e1a015f94e69c64f7cf3918f0288b15508b47dc
Haipethdev <haipethdev@gmail.com>
2019-08-14 13:44:28 +0100

    added base stats for warrior and abstract class

--- src/model/class_.py
+++ src/model/class_.py
@@ -1,15 +1,75 @@
 from abc import ABC
 
 class Class_(ABC):
+    
+    @property
+    @abstractmethod
+    def sta_to_hp(self):
+        return 10
+
+    @property
+    @abstractmethod
+    def armour_per_agi(self):
+        return 2
+
+    @property
+    @abstractmethod
+    def mana_per_int(self):
+        return 15
 
     @property
     @abstractmethod
     def power(self):
         pass
+    
+    @property
+    @abstractmethod
+    def base_hp_60(self):
+        pass
 
     @property
     @abstractmethod
-    def stats_per_level(self):
+    def base_power_60(self):
+        pass
+    
+    @property
+    @abstractmethod
+    def base_stats_60(self):
+        pass
+
+    @property
+    @abstractmethod
+    def base_dodge(self):
+        pass
+
+    @property
+    @abstractmethod
+    def dodge_per_agi(self):
+        pass
+
+    @property
+    @abstractmethod
+    def ap_per_str(self):
+        pass
+
+    @property
+    @abstractmethod
+    def rap_per_agi(self):
+        pass
+
+    @property
+    @abstractmethod
+    def crit_per_agi(self):
+        pass
+
+    @property
+    @abstractmethod
+    def hp5_per_spi(self):
+        pass
+  
+    @property
+    @abstractmethod
+    def base_hp5(self):
         pass
 
     @property
--- src/model/warrior.py
+++ src/model/warrior.py
@@ -1,6 +1,18 @@
 class Warrior(Class_):
     
-    __power = Power(rage)
-    __stats_per_level = {
-        2: Stats( 
-    
+    __power = Power("rage")
+    __base_hp_60 = 1689
+    __base_power_69 = 100
+    __base_stats_60 = Stats(100, 60, 90, 10, 25)
+    __base_dodge = 0
+    __dodge_per_agi = 1.0/20.0
+    __ap_per_str = 2
+    __rap_per_agi = 1
+    __crit_per_agi = 1.0/20.0
+    __hp5_per_spi = 0.8
+    __base_hp5 = 6
+    __weapon_proficiencies = {"swords", "maces", "axes", "2h swords", "2h maces", "2h
+axes", "daggers", "staves", "polearms", "bows", "guns", "crossbows", "thrown"}
+    __armour_proficiencies = {"cloth", "leather", "mail", "plate"}
+
+
